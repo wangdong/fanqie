ed8aa17a69c19e92a133e26e458c1550adc77778
wangdong <wangdong@kingsoft.com>
2010-12-24 16:50:03 +0800

    fanqie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanqie::Application.config.secret_token = '89251f41c7789682f8cf320ddd4a9fe3e172c0194ed932e7405a615a95cc29a70ebb4cbee4ae6d3898a28165f0cbfd863ba38c609371f2fd0d63a34b6dad7259'
